1811e798b8b37682ed4fe8b2a099dfb72d8cb4b4
ELIAS K PHILIP <58168391+Elias-Kurian-Philip@users.noreply.github.com>
2023-12-31 19:48:21 +0530

    Added a few more basic commands

--- Basic_Linux_Commands.sh
+++ Basic_Linux_Commands.sh
@@ -22,3 +22,49 @@ su
 
 echo
 #       echo - outputs the arguements separated by spaces followed by a newline.
+
+help
+#       help - display helpful information about built-in commands.
+
+touch
+#       touch - update the access and the modification times of each file to the current time. 
+#       A file arguement that does not exist is created empty.
+
+cat 
+#       cat - concatenate files and print on the standard output.
+
+whoami
+#       whoami - prints effective userID.
+
+hostname
+#       hostname - show or set the system's hostname.
+
+hostnamectl
+#       hostnamectl - maybe used to query and change the system's hostname and related settings.
+
+clear
+#       clears your screen if this is possible.
+
+vim
+#       it can be used to edit all kinds of plain text.
+
+rm
+#       remove files or directories.
+
+find
+#       search for files in a directory.
+
+mv
+#       move or rename files.
+
+cp
+#       copy files and directories.
+
+head
+#       head - output the first part of files.  
+
+tail 
+#       tail - output the last part of files.   
+
+grep
+#       grep - print lines that match PATTERN.  By default, grep displays the matching lines.
